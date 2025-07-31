04e5d855ed04a2496f7c3e0ab7a3672f541d96cb
3asyice <guilhermeabreu294@gmail.com>
2025-07-30 21:47:36 -0300

    update

--- service-worker.js
+++ service-worker.js
@@ -11,3 +11,22 @@ self.addEventListener("fetch", (event) => {
   // Aqui você pode personalizar o comportamento offline, se desejar
 });
 
+
+// notification 
+if ('mediaSession' in navigator) {
+  navigator.mediaSession.metadata = new MediaMetadata({
+    title: 'Rádio FM609',
+    artist: 'Transmissão ao vivo',
+    album: 'FM609 Playlist',
+    artwork: [
+      { src: 'capa.jpg', sizes: '512x512', type: 'image/jpeg' }
+    ]
+  });
+
+  navigator.mediaSession.setActionHandler('play', () => {
+    audio.play();
+  });
+  navigator.mediaSession.setActionHandler('pause', () => {
+    audio.pause();
+  });
+}
